2bafa75bc157d24e4c0fa0324c4bab8783a162e1
Regina Compton <reginafcompton@gmail.com>
2020-03-18 16:14:14 -0400

    test: Write test that insures that _check_for_required was called

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,17 +1,27 @@
+import pytest
+
 from converter.converter import Converter
 
-def test_converter_check_for_required(work_based_input_kwargs):
-    required_keywords = [
-        "program_description",
-        "program_name",
-        "program_url",
-        "provider_address",
-    ]
+@pytest.mark.parametrize("input_kwargs,required_keyword,should_error", [
+    ({"a_recommended_keyword": "value"}, "a_required_keyword", True),
+    ({"a_required_keyword": "value"}, "a_required_keyword", False)
+])
+def test_check_for_required(input_kwargs,required_keyword, should_error):
+    required_keywords = [required_keyword]
     
     converter = Converter(
-        data_keywords_mapper={},
+        data_keywords_mapper={"all": []},
         kwarg_to_schema_key_mapper={},
         required_keywords=required_keywords
     )
 
-    converter.trigger_conversion(work_based_input_kwargs)
+    if should_error:
+        with pytest.raises(RuntimeError) as exceptionMsg:
+            output = converter.trigger_conversion(input_kwargs)
+        
+        assert "One or more required properties needs to be included in the kwargs" in str(exceptionMsg.value)
+        assert required_keyword in str(exceptionMsg.value)
+    else:
+        output = converter.trigger_conversion(input_kwargs)
+
+        assert output == {}
