dcdefad8f92bbbcc1bc32dd3df32d50ceb53a3b9
Regina Compton <reginafcompton@gmail.com>
2020-03-18 16:20:07 -0400

    task: Add one more test case

--- converter/converter.py
+++ converter/converter.py
@@ -21,7 +21,7 @@ class Converter():
         missing_kwargs = [kwarg for kwarg in self.required_keywords if kwarg not in kwargs.keys()]
 
         if missing_kwargs:
-            missing_kwargs_as_str = ", ".join(missing_kwargs)
+            missing_kwargs_as_str = ",".join(missing_kwargs)
             raise RuntimeError(f"One or more required properties needs to be included in the kwargs: {missing_kwargs_as_str}")
 
     def _add_basic_keywords(self, kwargs):
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -2,13 +2,12 @@ import pytest
 
 from converter.converter import Converter
 
-@pytest.mark.parametrize("input_kwargs,required_keyword,should_error", [
-    ({"a_recommended_keyword": "value"}, "a_required_keyword", True),
-    ({"a_required_keyword": "value"}, "a_required_keyword", False)
+@pytest.mark.parametrize("input_kwargs,required_keywords,should_error", [
+    ({"a_recommended_keyword": "value"}, ["a_required_keyword"], True),
+    ({"a_recommended_keyword": "value"}, ["a_required_keyword, another_required_keyword"], True),
+    ({"a_required_keyword": "value"}, ["a_required_keyword"], False)
 ])
-def test_check_for_required(input_kwargs,required_keyword, should_error):
-    required_keywords = [required_keyword]
-    
+def test_check_for_required(input_kwargs,required_keywords, should_error):
     converter = Converter(
         data_keywords_mapper={"all": []},
         kwarg_to_schema_key_mapper={},
@@ -18,9 +17,9 @@ def test_check_for_required(input_kwargs,required_keyword, should_error):
     if should_error:
         with pytest.raises(RuntimeError) as exceptionMsg:
             output = converter.trigger_conversion(input_kwargs)
-        
+
         assert "One or more required properties needs to be included in the kwargs" in str(exceptionMsg.value)
-        assert required_keyword in str(exceptionMsg.value)
+        assert ",".join(required_keywords) in str(exceptionMsg.value)
     else:
         output = converter.trigger_conversion(input_kwargs)
 
